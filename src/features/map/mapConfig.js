6c872cc03be31de0cc12c39bdaff2d9c03b089e7
ravindu <ravindu.peiris11@gmail.com>
2026-03-29 16:45:43 +0530

    create mapconfig

---
+++ src/features/map/mapConfig.js
@@ -0,0 +1,3 @@
+/** Shared MapTiler style for MapLibre (Map Tracking + Risk Map). */
+export const MAPTILER_KEY = 'vJhZing971Ammszt9jVW';
+export const MAP_STYLE = `https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_KEY}`;
